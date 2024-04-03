d75dc553bdfb36463d686cf2ea462958b5bddc4d
Kurai <fxt.kurai@gmail.com>
2024-04-03 12:38:48 -0400

    Action, Condition and Decision Tree scripts updated

--- GAME3001_Lab7_AguirreThomas/Assets/_MyAssets/_Scripts/DecisionTree/Actions/AttackAction.cs
+++ GAME3001_Lab7_AguirreThomas/Assets/_MyAssets/_Scripts/DecisionTree/Actions/AttackAction.cs
@@ -2,8 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// TODO: Fill in for Lab 7a.
+
 public class AttackAction : ActionNode
 {
-    
+    public AttackAction()
+    {
+        name = "Attack Action";
+    }
+
+    public override void Action()
+    {
+        // Enter Functionality for action
+        if(Agent.GetComponent<Starship>().state != ActionState.ATTACK)
+        {
+            Debug.Log("Starting " + name);
+            Starship ss = Agent.GetComponent<Starship>();
+            ss.state = ActionState.ATTACK;
+
+            //custom enter action
+        }
+
+        // Every frame
+        Debug.Log("Performing " + name);
+    }
 }
--- GAME3001_Lab7_AguirreThomas/Assets/_MyAssets/_Scripts/DecisionTree/Actions/MoveToPlayerAction.cs
+++ GAME3001_Lab7_AguirreThomas/Assets/_MyAssets/_Scripts/DecisionTree/Actions/MoveToPlayerAction.cs
@@ -2,8 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// TODO: Fill in for Lab 7a.
+
 public class MoveToPlayerAction : ActionNode
 {
-    
+    public MoveToPlayerAction()
+    {
+        name = "Move to Player Action";
+    }
+
+    public override void Action()
+    {
+        if (Agent.GetComponent<Starship>().state != ActionState.MOVE_TO_PLAYER)
+        {
+            Debug.Log("Starting " + name);
+            Starship ss = Agent.GetComponent<Starship>();
+            ss.state = ActionState.MOVE_TO_PLAYER;
+
+        }
+
+        Debug.Log("Performing " + name);
+    }
 }
